fc793745c5557c1e73885dfc84ca1f2b09ed2f52
Subhadeep Pal <subhadeepal@gmail.com>
2017-10-05 23:50:03 +0530

    placeholder for text view pod added and how to use

--- Pods/Target Support Files/Pods-Health App/Pods-Health App-frameworks.sh
+++ Pods/Target Support Files/Pods-Health App/Pods-Health App-frameworks.sh
@@ -103,9 +103,11 @@ strip_invalid_archs() {
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/LGSideMenuController/LGSideMenuController.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/UITextView+Placeholder/UITextView_Placeholder.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/LGSideMenuController/LGSideMenuController.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/UITextView+Placeholder/UITextView_Placeholder.framework"
 fi
 if [ "${COCOAPODS_PARALLEL_CODE_SIGN}" == "true" ]; then
   wait
